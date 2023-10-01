15d8337c1ef8759cbe955342170f47822d2623e1
Ricardo Gorki <ricardo.gorki@gmail.com>
2023-10-01 17:23:04 -0300

    fix sqlite error

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,10 @@
-import fastify from 'fastify'
-import { knex } from 'knex'
-import { randomUUID } from 'node:crypto'
+import { app } from './app'
 import { env } from './env'
 
-const server = fastify()
-
-server.get('/hello', async () => {
-  const transactions = await knex('transactions').select('*')
-
-  return transactions
-})
-server.get('/hellos', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
-  return transaction
-})
-
-server.listen({ port: env.PORT }).then(() => {
-  console.log('Running!')
-})
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
